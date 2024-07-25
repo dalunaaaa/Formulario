dbbbeaeb5c0976dfa2963aadd6629b282197e519
danialuna <dmluna@scl.edu.gt>
2024-07-25 10:04:12 -0600

    envio la rama diseñologin a git para asegurar el trabajo

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ div_formulario.className = "div_formularios";
 
 //lista de datos String
 let formularios = [
-    {nombre:"Login", link:"https:"},
-    {nombre:"Login", link:"https:"},
-    {nombre:"Login", link:"https:"}, 
-    {nombre:"Login", link:"https:"},
+    {nombre:"Login", link:"componentes/login/login.html"},
+    {nombre:"Login", link:"#"},
+    {nombre:"Login", link:"#"}, 
+    {nombre:"Login", link:"#"},
     
 ];
 
